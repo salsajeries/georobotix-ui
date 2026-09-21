1dd70b1091626b683c55b6a6c5cd135a03237c39
Salwa Jeries <annette.jeries@gmail.com>
2026-09-21 11:41:25 -0500

    Setup docs with vue and vuetify

---
+++ docs/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="pa-8">
+        <h1 class="text-h4 mb-6">
+          GeoRobotix UI
+        </h1>
+
+        <v-btn color="primary">
+          Test Button
+        </v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
